41ceb0b8f827e9fe583b28c087b80b194143ac6a
Agustin <agustinspaccesi@gmail.com>
2023-12-28 16:05:42 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.radio
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:radio/app/app.dart';
+import 'package:radio/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
